46206aea86ff353ff2ed52aafd4cf07004da91ac
kristiandiana <kristian8diana@gmail.com>
2024-01-13 15:21:06 -0500

    react to flask connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import InputForm from './InputForm';
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/get").then(
+      res => {
+        return res.json();
+      }
+    ).then(
+      data => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+  const handleFormSubmit = (inputValue) => {
+
+    axios.post("http://localhost:5000/api/submit", { inputValue })
+      .then(response => {
+          console.log('Backend response:', response.data);
+          window.location.reload(false)
+      })
+      .catch(error => {
+          console.error('Error submitting input:', error);
+      });
+  };
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <InputForm onSubmit={handleFormSubmit} />
+
+      {(typeof data.response === 'undefined') ? (
+           <p>Loading...</p>
+        ) : (
+          data.response.map((item, i) => (
+            <p key={i}>{item}</p>
+          ))
+        )}
+
+
+        
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
--- server/server.py
+++ server/server.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify, redirect, url_for
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+CORS(app, origins=["http://localhost:5173"])
+
+user_input = ""
+from flask import Response
+
+@app.route("/api/get", methods=['GET'])
+def get_data():
+    global user_input
+    return {"response": [user_input]}
+
+@app.route("/api/submit", methods=['POST', 'GET'])
+def submit_data():
+    input_data = request.json.get('inputValue')
+    print("received info")
+    global user_input
+    user_input = input_data + " response from backend"
+    get_data()
+    return {"response": ["response:", input_data]}
 
 if __name__ == '__main__':
     app.run()
