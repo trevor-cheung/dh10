c31dc09b10126778f18f20348cf7e71d98d3b2af
kristiandiana <kristian8diana@gmail.com>
2024-01-14 03:23:25 -0500

    rough copy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Col, Row, Container } from 'reactstrap'
 import { Button,Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css"
 import Sidebar from '../components/SideBar';
 import Search from '../components/Search';
-
+import axios from 'axios'
 
 
 
@@ -13,6 +13,63 @@ const Home = (args) => {
   const [modal, setModal] = useState(false);
 
   const toggle = () => setModal(!modal);
+
+
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/get").then(
+      res => {
+        return res.json();
+      }
+    ).then(
+      data => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+
+  const [summary, setSummary] = useState([{}]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/get_summary").then(
+      res => {
+        return res.json();
+      }
+    ).then(
+      summary => {
+        setSummary(summary);
+        console.log(summary);
+      });
+  }, []);
+
+
+  const [image_data, setImageData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/get_image").then(
+      res => {
+        return res.json();
+      }
+    ).then(
+      image_data => {
+        setImageData(image_data);
+        console.log(image_data);
+      });
+  }, []);
+
+  const handleFormSubmit = (inputValue) => {
+
+    axios.post("http://localhost:5000/api/submit", { inputValue })
+      .then(response => {
+          console.log('Backend response:', response.data);
+          window.location.reload(false)
+      })
+      .catch(error => {
+          console.error('Error submitting input:', error);
+      });
+  };
 
   return (
 
@@ -26,7 +83,7 @@ const Home = (args) => {
           <div className="p-2 flex-grow-1 text-center">
             <div className="flex-column text-center">
               <h1 className='p-3'>I Need to Recycle...</h1>
-              <Search />
+              <Search onSubmit={handleFormSubmit}/>
               <div>
       <Button outline className="bi bi-question-circle m-3 px-2"  onClick={toggle}>
 
@@ -49,6 +106,31 @@ const Home = (args) => {
           </Button>
         </ModalFooter>
       </Modal>
+
+
+      {(typeof data.response === 'undefined') ? (
+           <p>Loading...</p>
+        ) : (
+          data.response.map((item, i) => (
+            <p key={i}>{item}</p>
+          ))
+        )}
+
+      {(typeof summary.response === 'undefined') ? (
+           <p>Loading...</p>
+        ) : (
+          summary.response.map((item, i) => (
+            <p key={i}>{item}</p>
+          ))
+        )}
+        {(typeof image_data.response === 'undefined') ? (
+           <p>Loading...</p>
+        ) : (
+          image_data.response.map((item, i) => (
+            <p key={i}><img src={item} alt="loading image..."></img></p>
+          ))
+        )}
+
 
     </div>
             </div>
